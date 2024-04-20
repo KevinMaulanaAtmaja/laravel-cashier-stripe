eac254c5d0e3b577c646765afdfbca5c014818f0
Kevin Maulana Atmaja <j04719671@gmail.com>
2024-04-20 21:24:55 +0700

    "sudah subs" alert

--- resources/views/subscriptions/plans.blade.php
+++ resources/views/subscriptions/plans.blade.php
@@ -2,6 +2,9 @@
 @section('konten')
 <div class="container d-grid justify-content-center">
     <h2 class="text-info">Subscribe Plans</h2>
+    @if (auth()->user()->stripe_id)
+        <div class="alert alert-success text-center">Sudah Subscribe</div>
+    @endif
     <div class="row">
         @foreach ($plan as $p)
         <div class="col-12 col-md-auto">
